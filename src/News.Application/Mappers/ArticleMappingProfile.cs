e6d14f3e812bac7f6c4582983c8e9b5ee6b2aeda
Hứa Đức Bình <binhbobinhbo22@gmail.com>
2024-08-04 13:43:44 +0700

    [Add] Add CQRS pattern in News.Application

---
+++ src/News.Application/Mappers/ArticleMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using News.Application.Responses;
+using News.Domain.Entities;
+
+namespace News.Application.Mappers
+{
+    public class ArticleMappingProfile : Profile
+    {
+        public ArticleMappingProfile()
+        {
+            CreateMap<Article, ArticleResponse>().ReverseMap();
+        }
+    }
+}
---
+++ src/News.Application/Responses/ArticleResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace News.Application.Responses
+{
+    public class ArticleResponse
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PubliccationDate { get; set; }
+        public string ImageUrl { get; set; }
+        public int Priority { get; set; }
+        public string UserId { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
